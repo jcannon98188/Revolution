bdab6fc5a26a24585d195d6a0449173fd8d6420b
Jason Cannon <jcannon98188@gmail.com>
2017-12-04 14:44:55 -0800

    More work on the Pooling

--- Revolution/Pool.cs
+++ Revolution/Pool.cs
@@ -6,8 +6,26 @@ using System.Threading.Tasks;
 
 namespace Revolution
 {
+    public enum PoolResults {
+        Ok,
+        ObjectAlreadyExists
+    }
+
+
+public class ComponentDoesNotExistException<T> : Exception
+    {
+        public ComponentDoesNotExistException(int entity) : base($"Component {typeof(T).Name} not found on Entity {entity}") { }
+    }
+
     public class Pool<T> where T : IPoolable, new()
     {
+        public static Pool<T> Instance { get
+            {
+                if (_instance == null) _instance = new Pool<T>();
+                return _instance;
+            }
+        }
+        private static Pool<T> _instance;
         private Stack<T> _items = new Stack<T>();
         private Dictionary<int, T> componentsInUse = new Dictionary<int, T>();
         private object _sync = new object();
@@ -21,15 +39,34 @@ namespace Revolution
                 {
                     return item;
                 }
-                if (_items.Count == 0)
-                {
-                    return new T();
-                }
-                return _items.Pop();
+                throw new ComponentDoesNotExistException<T>(entity);
             }
         }
 
-        public void Free(int entity)
+        public PoolResults Create(int entity)
+        {
+            lock (_sync)
+            {
+                T item;
+                if (!componentsInUse.TryGetValue(entity, out item))
+                {
+
+                    if (_items.Count == 0)
+                    {
+                        item = new T();
+                    }
+                    else
+                    {
+                        item = _items.Pop();
+                    }
+                    componentsInUse[entity] = item;
+                    return PoolResults.Ok;
+                }
+                return PoolResults.ObjectAlreadyExists;
+            }
+        }
+
+        public PoolResults Free(int entity)
         {
             lock(_sync)
             {
@@ -40,6 +77,7 @@ namespace Revolution
                     _items.Push(item);
                     componentsInUse.Remove(entity);
                 }
+                return PoolResults.Ok;
             }
         }
     }
