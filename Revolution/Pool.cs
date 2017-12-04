34a37910244e4f0595237e215aa1ec0ca88b534c
Jason Cannon <jcannon98188@gmail.com>
2017-12-04 14:23:55 -0800

    Adding Object Pooling

---
+++ Revolution/Pool.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revolution
+{
+    public class Pool<T> where T : IPoolable, new()
+    {
+        private Stack<T> _items = new Stack<T>();
+        private Dictionary<int, T> componentsInUse = new Dictionary<int, T>();
+        private object _sync = new object();
+
+        public T Get(int entity)
+        {
+            lock(_sync)
+            {
+                T item;
+                if (componentsInUse.TryGetValue(entity, out item))
+                {
+                    return item;
+                }
+                if (_items.Count == 0)
+                {
+                    return new T();
+                }
+                return _items.Pop();
+            }
+        }
+
+        public void Free(int entity)
+        {
+            lock(_sync)
+            {
+                T item;
+                if (componentsInUse.TryGetValue(entity, out item))
+                {
+                    item.Release();
+                    _items.Push(item);
+                    componentsInUse.Remove(entity);
+                }
+            }
+        }
+    }
+}
